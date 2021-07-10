12f924707163bbccedbe623caff4da4b189743bb
Kwakyejin <81745747+Kwakyejin@users.noreply.github.com>
2021-07-10 19:48:18 +0900

    Delete KWAILAB1 directory

--- KWAILAB1/search.m
+++
@@ -1,4 +0,0 @@
-function frame_mid = search(r,c)
-    turn(r,deg2rad(0));
-    [frame_mid,~] = snapshot(c);  
-end
--- KWAILAB1/shake.m
+++
@@ -1,9 +0,0 @@
-function [frame_mid,frame_right,frame_left] = shake(r,c)
-    turn(r,deg2rad(0));
-    [frame_mid,~] = snapshot(c);
-    turn(r,deg2rad(40));
-    [frame_right,~] = snapshot(c);
-    turn(r,deg2rad(-80));
-    [frame_left,~] = snapshot(c);
-    turn(r,deg2rad(40));
-end
